2f23149a1765a623ab57a4281c788f18faff1e7f
Shiping Yi <esp132@gmail.com>
2018-02-19 18:34:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,14 @@
 # code your #valid_move? method here
 def valid_move?()
+  if position_taken?(board,index)
 
+  else
+
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken(board,index)
+def position_taken?(board,index)
 if board[index]==nil || board[index]==""|| board[index]==" "
   false
 else
